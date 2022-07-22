8b9ed37cb9be5dac7558c73a94cca09583af89d8
Lorenzo Lewis <lorenzo_lewis@icloud.com>
2022-07-22 21:57:16 +0100

    Cleanup titlebar and add lable

--- src/lib/Titlebar.svelte
+++ src/lib/Titlebar.svelte
@@ -12,26 +12,27 @@
     ? `box-shadow: 0px 6px 4px -6px rgba(0, 0, 0, 0.1)`
     : ""}
 >
-  <div />
-
+  <!-- Leading edge -->
   <div class="vstack">
     <span style="font-weight: bold;">All </span>
     <span>{Intl.NumberFormat().format(symbolCount)} Symbols</span>
   </div>
 
-  <div />
-
-  <select bind:value={fontWeight}>
-    <option value={100}>Thin</option>
-    <option value={200}>Ultra Light</option>
-    <option value={300}>Light</option>
-    <option value={400} default>Regular</option>
-    <option value={500}>Medium</option>
-    <option value={600}>Semi Bold </option>
-    <option value={700}>Bold</option>
-    <option value={800}>Heavy</option>
-    <option value={900}>Black</option>
-  </select>
+  <!-- Trailing edge -->
+  <div class="trailing">
+    <label for="font-weight">Symbol Weight</label>
+    <select bind:value={fontWeight} id="font-weight">
+      <option value={100}>Thin</option>
+      <option value={200}>Ultra Light</option>
+      <option value={300}>Light</option>
+      <option value={400} default>Regular</option>
+      <option value={500}>Medium</option>
+      <option value={600}>Semi Bold </option>
+      <option value={700}>Bold</option>
+      <option value={800}>Heavy</option>
+      <option value={900}>Black</option>
+    </select>
+  </div>
 </header>
 
 <style>
@@ -48,20 +49,31 @@
     width: 100vw;
     position: fixed;
     height: 48px;
-    display: grid;
+    display: flex;
     justify-content: space-between;
     align-items: center;
     transition: box-shadow 150ms;
-    grid-template-columns: 80px max-content 1fr min-content 1fr;
     z-index: 100;
     background-color: var(--titlebar-background);
     -webkit-backdrop-filter: blur(20px);
     backdrop-filter: blur(10px);
+    font-size: 0.9rem;
+    line-height: 1.1rem;
   }
 
   header > * {
     user-select: none;
     pointer-events: none;
+  }
+
+  .vstack {
+    display: flex;
+    flex-direction: column;
+    padding-left: 80px;
+  }
+
+  .trailing {
+    padding-right: 2rem;
   }
 
   select {
@@ -71,12 +83,5 @@
 
   select:focus {
     outline: none;
-  }
-
-  .vstack {
-    font-size: 0.9rem;
-    line-height: 1.1rem;
-    display: flex;
-    flex-direction: column;
   }
 </style>
