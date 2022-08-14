4389570d3ca941547354295de573d32e226bd5f9
Jonas Kruckenberg <iterpre@protonmail.com>
2022-08-14 19:59:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,23 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	// prevent vite from obscuring rust errors
+	clearScreen: false,
+	// Tauri expects a fixed port, fail if that port is not available
+	server: {
+	  strictPort: true,
+	},
+	// to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+	// `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+	// env variables
+	envPrefix: ['VITE_', 'TAURI_'],
+	build: {
+	  // Tauri supports es2021
+	  target: ['es2021', 'chrome100', 'safari13'],
+	  // don't minify for debug builds
+	  minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+	  // produce sourcemaps for debug builds
+	  sourcemap: !!process.env.TAURI_DEBUG,
+	},
+	plugins: [sveltekit()],
 });
