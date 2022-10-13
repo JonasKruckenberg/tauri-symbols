c10158c1629a1df0a4d893355dcbbd5490d07db3
Jonas Kruckenberg <iterpre@protonmail.com>
2022-10-13 17:45:45 +0200

    fix isolation app

--- isolation/index.js
+++ isolation/index.js
@@ -1,11 +1,11 @@
 window.__TAURI_ISOLATION_HOOK__ = (payload) => {
     // let's validate the regex pattern here to make sure it's present and a valid pattern
 	if (payload.cmd === 'search') {
-		if (!('pattern' in payload)) return new Error('expected pattern key in payload');
+		if (!('pattern' in payload)) return null;
 
 		try {
 			new RegExp(payload.pattern);
-			return null;
+			return payload;
 		} catch (e) {
 			return null;
 		}
